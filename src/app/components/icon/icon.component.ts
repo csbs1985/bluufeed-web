910edaead4893e674d43ba2467e09592add46d7b
Charles Santos <csbs.conta@outlook.com>
2022-09-26 13:21:24 -0300

    [projeto] criar

---
+++ src/app/components/icon/icon.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+const BASE_PATH = 'assets/icons';
+
+@Component({
+  selector: 'bf-icon',
+  templateUrl: './icon.component.html',
+  styleUrls: ['./icon.component.scss'],
+})
+export class IconComponent {
+  @Input() size: number = 24;
+
+  public iconPath!: string;
+
+  @Input() set icon(value: string) {
+    this.iconPath = `${BASE_PATH}/${value}.svg`;
+  }
+}
